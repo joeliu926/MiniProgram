6c1cd97a5ed625845698c8d75d1844bb79cafc36
yongchao71 <yongchao71@163.com>
2017-11-14 08:40:54 +0800

    add interface

--- pages/pcase/citem/citem.js
+++ pages/pcase/citem/citem.js
@@ -5,6 +5,18 @@ Page({
    * 页面的初始数据
    */
   data: {
+    caseList: [
+      {
+        "id": 1,
+        "doctorName": "刘德华",
+        "customerLogo": "http://101.132.161.222:8077/mc_files/10088/CASE_LIBRARY/7cb3da9e-f690-4b79-b32c-6a69bdcf629b",
+        "customerName": "吴彦祖",
+        "caseName": "我要隆个鼻",
+        "productName": "你猜",
+        "frondFile": "http://101.132.161.222:8077/mc_files/10088/CASE_LIBRARY/7cb3da9e-f690-4b79-b32c-6a69bdcf629b",
+        "backFile": "http://101.132.161.222:8077/mc_files/10088/CASE_LIBRARY/7da37ca3-dde3-4d9a-ab9b-f40c181c4b83"
+      }
+    ],
     imgUrls: [
       'http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg',
       'http://img06.tooopen.com/images/20160818/tooopen_sy_175866434296.jpg',
@@ -22,6 +34,7 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+    console.log(options);
     var test = options.test;
     if (test) {
       this.setData({
@@ -109,6 +122,26 @@ Page({
  
     wx.showShareMenu({
       withShareTicket: true
+    });
+  },
+  getCaseList(param) {
+    let _This = this;
+    wx.request({
+      url: "https://27478500.qcloud.la/wxa/product/list",
+      method: "POST",
+      data: {
+        unionId: param
+      },
+      header: {
+        'Content-Type': 'application/json'
+      },
+      success: function (result) {
+        if (result.data.code == 0) {
+          _This.setData({ projectItems: result.data.data });
+        } else {
+          console.log(result);
+        }
+      }
     });
   }
 })

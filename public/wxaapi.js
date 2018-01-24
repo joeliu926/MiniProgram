265ce55f24fcd37bc65a3a18a9a197a06320d96d
yiweidujiang <yiweidujiang869527@163.com>
2018-01-24 16:47:19 +0800

    bug xiugai

--- public/wxaapi.js
+++ public/wxaapi.js
@@ -15,7 +15,7 @@ var gConfig={
 
   /****uat end*****/
    /****dev start*****/
-  remoteWx: "https://nihaomc.com/wx",
+  remoteWx: "https://nihaomc.com/wx_test",
   remote: "https://nihaomc.com/wxa_dev",
   uploadUrl: "https://nihaomc.com/uploadimg_test/attachment/upload"
   /****dev end*****/

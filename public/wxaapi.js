456772aad1fb290e7eaccc0c5552dc999b44921d
yiweidujiang <yiweidujiang869527@163.com>
2018-01-24 16:31:50 +0800

    000wxappi.js

--- public/wxaapi.js
+++ public/wxaapi.js
@@ -14,7 +14,7 @@ var gConfig={
 
   /****uat end*****/
  
-  /****dev start*****/
+   /****dev start*****/
   // remoteWx: "https://nihaomc.com/wx",
   // remote: "https://nihaomc.com/wxa_dev",
   // uploadUrl: "https://nihaomc.com/uploadimg_test/attachment/upload"

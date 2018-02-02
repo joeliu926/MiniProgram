63fd7fa12e3238dacc0685f23bb5578d3bb30c09
yiweidujiang <yiweidujiang869527@163.com>
2018-02-02 13:18:15 +0800

    xiugaiyangshi

--- public/wxaapi.js
+++ public/wxaapi.js
@@ -2,6 +2,7 @@ var gConfig={
 
   remoteWx: "https://nihaomc.com/wx_test",
   remote: "https://nihaomc.com/wxa_dev",
+  // remote: "https://nihaomc.com/wxa_test",
   uploadUrl: "https://nihaomc.com/uploadimg_test/attachment/upload"
 
 }

36b39e66680b671c790adc5a16f4eee6a88f67e9
yongchao71 <yongchao71@163.com>
2018-03-02 15:26:56 +0800

    change show type

--- pages/projectcase/caselist/caselist.js
+++ pages/projectcase/caselist/caselist.js
@@ -55,10 +55,6 @@ Page({
     likeCount: 0,
     isLikeItems: {},
     /////////////////////////////////////////////////////
-    indicatorDots: false,
-    autoplay: false,
-    interval: 5000,
-    duration: 500,
     isConsult: true,
     // 弹层
     uicondata: "",
@@ -67,7 +63,6 @@ Page({
     itemids: [],
     jSelect: "",
     isactive: false,
-    ishow: false,
     projectItems: [],
     sSelect: [],
     arrData: [],
@@ -75,9 +70,8 @@ Page({
     changeColor: "#999999",
     productlistArr: [],
     selable: [],
-    allarr: [],
     isShow: 'false',
-    current: 0,
+    isSelectProduct:false,//是否打开选择项目下拉
     productcodes: []
   },
 
@@ -85,13 +79,13 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+    //console.log("case list options-------", options, options.caseIds);
     var _This = this;
-    var itemids = options.itemids.split(",");
-    var caseIds = options.caseIds;
+    var itemids = options.itemids.split(",");//获取前一个页面的选中项目Code
     getApp().getUserData(function (uinfo) {
       _This.setData({
-        isConsult: caseIds ? false : true,
-        caseIds: caseIds || "",
+        isConsult:  true,
+        caseIds: "",
         projectName: options.iname,
         productCode: options.itemid,
         cstUid: uinfo.unionId,
@@ -105,6 +99,7 @@ Page({
         sSelect: itemids,
       });
       _This.fGetCaseList(uinfo);//获取案例
+      _This.fGetDropdownProductList();//获取下拉选项的项目列表
     });
     //设置第一个项目是选中的；
     this.data.arrData[0].changeColor = '#9083ed';
@@ -226,7 +221,7 @@ Page({
       type: _This.data.shareType // 
     };
 
-    console.log('shareData', shareData);
+    //console.log('shareData', shareData);
     wxRequest(wxaapi.consult.consultantupdate.url, shareData).then(function (result) {
       if (result.data.code == 0) {
         _This.setData({ projectItems: result.data.data });
@@ -300,15 +295,6 @@ Page({
     });
   },
   /**
-   * 滑动事件，改变当前的信息
-   */
-  fSwiperChange: function (e) {
-    this.setData({
-      currentPage: e.detail.current + 1
-    });
-  },
-
-  /**
    * 获取会话ID，咨询师获取会话ID进行消息分享
    */
   fGetConsultationId(sItem, callback) {
@@ -343,8 +329,6 @@ Page({
     var oData = _This.data.oEvent;
     oData.eventAttrs.triggeredTime = new Date().valueOf();
     oData.code = eType;
-
-    console.log('oData', oData);
     wxRequest(wxaapi.event.v2.url, oData).then(function (result) {
       if (result.data.code == 0) {
         if (!oData.shareEventId) {
@@ -374,74 +358,59 @@ Page({
           caseList: result.data.data,
           totalCount: result.data.data.length
         });
-        // console.log("case list----", _This.data.caseList);
+         //console.log("case list----", _This.data.caseList);
       } else {
         console.log("case list----", result);
       }
     });
   },
   /**
-   * 下拉选择 获取项目列表
+   * 获取下拉选项项目列表
    */
-  getproductitem() {
+  fGetDropdownProductList(){
     let _This = this;
-
-    if (this.data.itemids.length > 0) {
-      _This.setData({
-        isConsult: false,
-        isactive: true
-      })
-    }
-    //全部的项目================
+    //全部的项目================all:0是所有有案例的项目
     let pdata = { unionId: _This.data.oUserInfo.unionId };//,all:0
     wxRequest(wxaapi.product.list.url, pdata).then(function (result) {
+      //console.log("get product list result not all--------", result);
       if (result.data.code == 0) {
-        // 没有案例的项目不可用
-        var everyarr = [];
-        result.data.data.forEach(function (item) {
-          item.productList.forEach(function (oitem) {
-            oitem.productList.forEach(function (titem) {
-              // console.log(titem.productCode);
-              everyarr.push(titem.productCode);
-            })
-          })
-        })
         _This.setData({
           projectItems: result.data.data,
-          allarr: everyarr,
-          ishow: !_This.data.ishow,
-          isactive: _This.data.isactive,
         });
+        //可选的项目
+        let abledata = { unionId: _This.data.oUserInfo.unionId, all: 0 };//,all:0
+        return wxRequest(wxaapi.product.list.url, abledata);
       } else {
-        console.log(result);
+        return {data:{}};
       }
-      wx.hideLoading();
-    });
-
-    //可选的项目
-    let abledata = { unionId: _This.data.oUserInfo.unionId, all: 0 };//,all:0
-    wxRequest(wxaapi.product.list.url, abledata).then(function (result) {
-      // console.log("3333333333333333333===>", result.data.data);
+    }).then(function (result) {
+      //console.log("get product list result all=0--------", result);
       var codearr = [];
       if (result.data.code == 0) {
         result.data.data.forEach(function (item) {
           item.productList.forEach(function (oitem) {
             oitem.productList.forEach(function (titem) {
-              // console.log(titem.productCode);
               codearr.push(titem.productCode);
-
-            })
-          })
-        })
+            });
+          });
+        });
         _This.setData({
-          // projectItems: result.data.data,
           selable: codearr
         });
       } else {
         console.log(result);
       }
-      wx.hideLoading();
     });
+  },
+  /**
+   * 下拉选择 获取项目列表
+   */
+  fGetproductItem() {
+    let _This = this;
+      _This.setData({
+        isactive: true,
+        isSelectProduct:true
+      });
   },
   /**
    *选择项目切换样式 （顶部）
@@ -544,7 +513,7 @@ Page({
         _This.setData({
           caseList: result.data.data,
           totalCount: result.data.data.length,
-          ishow: !_This.data.ishow,
+          isSelectProduct: !_This.data.isSelectProduct,
           arrData: _This.data.arrData,
           itemids: _This.data.sSelect,
         });

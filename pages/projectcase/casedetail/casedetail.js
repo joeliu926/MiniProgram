ce41e0e7d7af8715223019705aca0db1835096e6
yongchao71 <yongchao71@163.com>
2018-03-08 13:28:34 +0800

    change detail

--- pages/projectcase/casedetail/casedetail.js
+++ pages/projectcase/casedetail/casedetail.js
@@ -159,7 +159,7 @@ Page({
    // _This.data.detailId=1;
     var pdata = {did: _This.data.detailId};
     wxRequest(wxaapi.pcase.detail.url, pdata).then(function (result) {
-      //console.log("detail0000000000===>", result);
+      console.log("detail0000000000===>", result);
       if (result.data.code == 0) {
         _This.setData({
           detailInfo: result.data.data
